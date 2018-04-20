7ec35024a121f61ca771985168eac12b507420a4
Robert Mariano Schwindaman <schwindy1234@gmail.com>
2018-04-20 03:22:53 -0600

    Hi, I am Skippy[0]

--- api/controllers/Balance/allBalances.js
+++ api/controllers/Balance/allBalances.js
@@ -6,21 +6,19 @@ const getItem = require('../LootSafe/getItem')
  * @route
  */
 module.exports = (address) => {
-  return simpleCall('LootSafe', 'getItems').then(items => {
     return simpleCall('LootSafe', 'getItems').then(data => {
-      return Promise.all([...data.map(item => getItem(item))]).then(items => {
-        return Promise.all([
-          ...items.map(item => {
-            return getInstance('Item', item.address).then(async instance => {
-              return instance.balanceOf.call(address).then(balance => {
-                return {
-                  [item.address]: balance
-                }
-              })
-            })
-          })
-        ])
-      })
+        return Promise.all([...data.map(item => getItem(item))]).then(items => {
+            return Promise.all([
+                ...items.map(item => {
+                    return getInstance('Item', item.address).then(async instance => {
+                        return instance.balanceOf.call(address).then(balance => {
+                            return {
+                                [item.address]: balance
+                            }
+                        })
+                    })
+                })
+            ])
+        })
     })
-  })
 }
